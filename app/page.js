de75e32497677a03a771bc624affc568741e6fed
Mohit Joping <mohitjoping1@gmail.com>
2024-08-09 12:41:40 +0530

    Fix: UI and file struture.

--- app/page.js
+++ app/page.js
@@ -1,6 +1,11 @@
 
 "use client";
-import { Box, Stack, TextField, Button } from "@mui/material";
+
+import Chatbot from "@/components/Chatbot";
+import { Divide } from "lucide-react";
+import { Main } from "next/document";
+// In this code we are using the MUI library so we made changes into this on 9th august 2024 so if it not runs don't worry. I will fix it soon. --mohitjoping
+// import { Box, Stack, TextField, Button } from "@mui/material";
 import { useState } from "react";
 
 export default function Home() {
@@ -76,65 +81,69 @@ export default function Home() {
   };
 
   return (
-    <Box
-      width="100vw"
-      height="100vh"
-      display="flex"
-      flexDirection="column"
-      justifyContent="center"
-      alignItems="center"
-    >
-      <Stack
-        direction={"column"}
-        width="500px"
-        height="700px"
-        border="1px solid black"
-        p={2}
-        spacing={3}
-      >
-        <Stack
-          direction={"column"}
-          spacing={2}
-          flexGrow={1}
-          overflow="auto"
-          maxHeight="100%"
-        >
-          {messages.map((message, index) => (
-            <Box
-              key={index}
-              display="flex"
-              justifyContent={
-                message.role === "assistant" ? "flex-start" : "flex-end"
-              }
-            >
-              <Box
-                bgcolor={
-                  message.role === "assistant"
-                    ? "primary.main"
-                    : "secondary.main"
-                }
-                color="white"
-                borderRadius={16}
-                p={3}
-              >
-                {message.content}
-              </Box>
-            </Box>
-          ))}
-        </Stack>
-        <Stack direction={"row"} spacing={2}>
-          <TextField
-            label="Message"
-            fullWidth
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            onKeyPress={handleKeyPress}
-          />
-          <Button variant="contained" onClick={sendMessage}>
-            Send
-          </Button>
-        </Stack>
-      </Stack>
-    </Box>
+    <div className="flex justify-center items-center border-gray-200 w-full">
+ <Chatbot />
+    </div>
+   
+    // <Box
+    //   width="100vw"
+    //   height="100vh"
+    //   display="flex"
+    //   flexDirection="column"
+    //   justifyContent="center"
+    //   alignItems="center"
+    // >
+    //   <Stack
+    //     direction={"column"}
+    //     width="500px"
+    //     height="700px"
+    //     border="1px solid black"
+    //     p={2}
+    //     spacing={3}
+    //   >
+    //     <Stack
+    //       direction={"column"}
+    //       spacing={2}
+    //       flexGrow={1}
+    //       overflow="auto"
+    //       maxHeight="100%"
+    //     >
+    //       {messages.map((message, index) => (
+    //         <Box
+    //           key={index}
+    //           display="flex"
+    //           justifyContent={
+    //             message.role === "assistant" ? "flex-start" : "flex-end"
+    //           }
+    //         >
+    //           <Box
+    //             bgcolor={
+    //               message.role === "assistant"
+    //                 ? "primary.main"
+    //                 : "secondary.main"
+    //             }
+    //             color="white"
+    //             borderRadius={16}
+    //             p={3}
+    //           >
+    //             {message.content}
+    //           </Box>
+    //         </Box>
+    //       ))}
+    //     </Stack>
+    //     <Stack direction={"row"} spacing={2}>
+    //       <TextField
+    //         label="Message"
+    //         fullWidth
+    //         value={message}
+    //         onChange={(e) => setMessage(e.target.value)}
+    //         onKeyPress={handleKeyPress}
+    //       />
+    //       <Button variant="contained" onClick={sendMessage}>
+    //         Send
+    //       </Button>
+    //     </Stack>
+    //   </Stack>
+    // </Box>
   );
 }
