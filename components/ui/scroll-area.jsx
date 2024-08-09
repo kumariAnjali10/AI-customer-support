9ddd6423c4033a1fc7ff087c46c72e39b3e3311e
Mohit Joping <mohitjoping1@gmail.com>
2024-08-10 00:48:29 +0530

    Feat: Chatbot is successfully developed

--- components/ui/scroll-area.jsx
+++ components/ui/scroll-area.jsx
@@ -3,14 +3,17 @@
 import * as React from "react"
 import * as ScrollAreaPrimitive from "@radix-ui/react-scroll-area"
 
-import { cn } from "@/lib/utils"
+import { cn } from "../../lib/utils"
 
 const ScrollArea = React.forwardRef(({ className, children, ...props }, ref) => (
   <ScrollAreaPrimitive.Root
-    ref={ref}
+    
     className={cn("relative overflow-hidden", className)}
     {...props}>
-    <ScrollAreaPrimitive.Viewport className="h-full w-full rounded-[inherit]">
+    <ScrollAreaPrimitive.Viewport 
+    className="h-full w-full rounded-[inherit]"
+    ref={ref}
+    >
       {children}
     </ScrollAreaPrimitive.Viewport>
     <ScrollBar />
