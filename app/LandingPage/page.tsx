ba9d8bc4b52e77ec9f376ccff464d53bf817b1f8
Kumari Anjali <kumarianjali206700@gmail.com>
2024-08-11 20:28:42 +0530

    "added few questions to chat file "

--- app/LandingPage/page.tsx
+++ app/LandingPage/page.tsx
@@ -9,7 +9,7 @@ export default function LandingPage() {
       <div className="flex justify-end pt-10 pr-10">
         {/* login button */}
         <button className="border text-sm font-medium relative border-neutral-200 dark:border-white/[0.2] text-white dark:text-white px-4 py-2 rounded-full">
-          <span>Login</span>
+          <span><a href="HomePage">Login</a></span>
           <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent  h-px" />
         </button>
       </div>
