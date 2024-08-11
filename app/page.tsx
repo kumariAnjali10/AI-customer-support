cb906dedd6e1dcb55537b1d373b2b0b9f97705aa
Suraj <suraj.ukumar.p@gmail.com>
2024-08-11 17:41:14 +0530

    file changed.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 // import { HeroSection } from "@/components/HeroSection";
 
 // import Chat from "./chat/page";
-import LandingPage from "./landingpage/page";
+import LandingPage from "./LandingPage/page";
 
 // import { LandingPage } from "@/components/LandingPage";
 
