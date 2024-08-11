3723c79001aed9f0c42e51ad70c77db3f6fdcbf0
Suraj <suraj.ukumar.p@gmail.com>
2024-08-11 17:40:47 +0530

    refactor(name): The folder names corrected.

---
+++ app/HomePage/page.tsx
@@ -0,0 +1,13 @@
+import { Navbar } from "@/components/Navbar";
+import { HeroSection } from "../../components/HeroSection";
+
+
+export default function HomePage() {
+    return (
+      <main className="">
+        <Navbar />
+        <HeroSection />
+        {/* <Chat /> */}
+      </main>
+    );
+  }
---
+++ app/LandingPage/page.tsx
@@ -0,0 +1,43 @@
+"use client";
+import react from "react";
+
+import { SparklesCore } from "../../components/ui/sparkles";
+
+export default function LandingPage() {
+  return (
+    <main className="bg-black min-h-screen">
+      <div className="flex justify-end pt-10 pr-10">
+        {/* login button */}
+        <button className="border text-sm font-medium relative border-neutral-200 dark:border-white/[0.2] text-white dark:text-white px-4 py-2 rounded-full">
+          <span>Login</span>
+          <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent  h-px" />
+        </button>
+      </div>
+      <div className="h-[40rem] w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
+        <h1 className="md:text-4xl text-4xl lg:text-8xl font-bold text-center text-white relative z-20">
+        AI Chatbot
+        </h1>
+        <div className="w-[40rem] h-40 relative">
+          {/* Gradients */}
+          <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
+          <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
+          <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
+          <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
+
+          {/* Core component */}
+          <SparklesCore
+            background="transparent"
+            minSize={0.4}
+            maxSize={1}
+            particleDensity={1200}
+            className="w-full h-full"
+            particleColor="#FFFFFF"
+          />
+
+          {/* Radial Gradient to prevent sharp edges */}
+          <div className="absolute inset-0  w-full h-full  bg-black [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
+        </div>
+      </div>
+    </main>
+  );
+}
