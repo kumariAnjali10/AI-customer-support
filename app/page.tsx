efb9a97c67ebfe1e802e308e2e0c50bc44249c58
Mohit Joping <mohitjoping1@gmail.com>
2024-08-11 16:15:31 +0530

    ui initialised succesfully

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+// import { HeroSection } from "@/components/HeroSection";
+import { Chat } from "@/components/Chatbot";
+import { HomePage } from "@/components/HomePage";
+// import { LandingPage } from "@/components/LandingPage";
+
+
+
+export default function Home() {
+  return (
+    <main className="">
+        {/* <HomePage /> */}
+        {/* <LandingPage /> */}
+        <Chat />  
+    </main>
+  );
+}
